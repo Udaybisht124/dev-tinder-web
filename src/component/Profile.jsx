03e7309b960b8899c92a435393862fd84f6f0996
Udaybisht124 <96695491+Udaybisht124@users.noreply.github.com>
2025-02-19 21:51:33 +0530

    Update Profile.jsx

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import EditProfile from "./EditProfile.jsx";
+import EditProfile from "./EditProfile";
 
 const Profile = () => {
   const user = useSelector((store) => store.user);
@@ -11,4 +11,5 @@ const Profile = () => {
     )
   );
 };
+
 export default Profile;
