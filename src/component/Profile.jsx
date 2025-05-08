3a88758f1f5653979e09c69a8bedf1a51bef1d1c
udaybisht124 <bishtuday4668@gmail.com>
2025-05-08 14:21:10 +0530

    update profile component

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import EditProfile from "./EditProfile";
+import EditProfile from "./Editprofile.jsx";
 
 const Profile = () => {
   const user = useSelector((store) => store.user);
